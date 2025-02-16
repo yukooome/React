fbb5e6cf25211ffc2f3e2b35c99086dedb912414
Ton Nom <maissaa.elbhi@gmail.com>
2025-02-16 12:18:58 +0100

    aj aut nav

--- wp-react/src/components/Navbar.jsx
+++ wp-react/src/components/Navbar.jsx
@@ -21,7 +21,6 @@
 
 // export default Navbar;
 
-
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
@@ -33,6 +32,11 @@ const Navbar = () => {
   // Fonction pour basculer l'état du menu
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
+  };
+
+  // Fonction pour fermer le menu
+  const closeMenu = () => {
+    setMenuOpen(false);
   };
 
   return (
@@ -47,13 +51,13 @@ const Navbar = () => {
 
       {/* Liens de navigation */}
       <ul className={`navbar-links ${menuOpen ? 'active' : ''}`}>
-        <li><Link to="/">Accueil</Link></li>
-        <li><Link to="/bases">Apprendre les bases</Link></li>
-        <li><Link to="/techniques-avancees">Techniques Avancées</Link></li>
-        <li><Link to="/anatomie">Anatomie</Link></li>
-        <li><Link to="/membre">Membre</Link></li>
-        <li><Link to="/position">Position</Link></li>
-        <li><Link to="/artistes">Artistes</Link></li>
+        <li><Link to="/" onClick={closeMenu}>Accueil</Link></li>
+        <li><Link to="/bases" onClick={closeMenu}>Apprendre les bases</Link></li>
+        <li><Link to="/techniques-avancees" onClick={closeMenu}>Techniques Avancées</Link></li>
+        <li><Link to="/anatomie" onClick={closeMenu}>Anatomie</Link></li>
+        <li><Link to="/membre" onClick={closeMenu}>Membre</Link></li>
+        <li><Link to="/position" onClick={closeMenu}>Position</Link></li>
+        <li><Link to="/artistes" onClick={closeMenu}>Artistes</Link></li>
       </ul>
     </nav>
   );
